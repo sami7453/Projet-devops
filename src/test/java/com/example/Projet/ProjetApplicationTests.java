9c1d3e87bd6c483962f70e806f358734c22c270c
sami7453 <145992280+sami7453@users.noreply.github.com>
2025-03-21 11:24:33 +0100

    Initial commit

---
+++ src/test/java/com/example/Projet/ProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
